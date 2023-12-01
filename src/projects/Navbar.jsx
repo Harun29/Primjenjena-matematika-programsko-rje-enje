cfc1da31c51854eff9c0e9062df540cbec661c5c
Harun29 <ibrahimovic.harun01@gmail.com>
2023-12-01 22:20:56 +0100

    made navbar on the left and styled it

--- src/projects/Navbar.jsx
+++ src/projects/Navbar.jsx
@@ -7,23 +7,33 @@ const Navbar = () => {
         <h1>PMIS</h1>
       </div>
       <div className="methods">
-        <h2>Methods</h2>
+        <h2>Metode</h2>
         <ul>
-          <li>
-            <Link to="/polovljenje">Metoda Polovljenja Intervala</Link>
-          </li>
-          <li>
-            <Link to="/regula-falsi">Metoda Regula Falsi</Link>
-          </li>
-          <li>
-            <Link to="/prosta-iteracija">Metoda Proste Iteracije</Link>
-          </li>
-          <li>
-            <Link to="/newton-raphson">Newton-Raphsonova Metoda</Link>
-          </li>
-          <li>
-            <Link to="/modifikovana-newtonova">Modifikovana Newtonova Metoda</Link>
-          </li>
+          <Link to="/polovljenje">
+            <li>
+              Metoda Polovljenja Intervala
+            </li>
+          </Link>
+          <Link to="/regula-falsi">
+            <li>
+              Metoda Regula Falsi
+            </li>
+          </Link>
+          <Link to="/prosta-iteracija">
+            <li>
+              Metoda Proste Iteracije
+            </li >
+          </Link>
+          <Link to="/newton-raphson">
+            <li>
+              Newton-Raphsonova Metoda
+            </li>
+          </Link>
+          <Link to="/modifikovana-newtonova">
+            <li>
+              Modifikovana Newtonova Metoda
+            </li>
+          </Link>
         </ul>
       </div>
     </div>
