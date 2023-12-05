b0120f641900628b2221d84b40602c535e763a8a
Harun29 <ibrahimovic.harun01@gmail.com>
2023-12-05 17:46:49 +0100

    cahnged description to Bosnian

--- src/projects/Naslov.jsx
+++ src/projects/Naslov.jsx
@@ -7,19 +7,19 @@ const Objasnjenje = ({ metoda }) => {
   useEffect(() => {
     if (metoda === "regula") {
       setNaslov("Metoda Regula Falsi");
-      setDescription("Ova metoda kombinuje ideje metode polovljenja i secirajuće tačke. Umesto uzimanja sredine intervala, koristi se tačka gde prava koja povezuje vrednosti funkcije na krajevima intervala seče x-osu. Ta tačka postaje novi kraj intervala.");
+      setDescription("Ova metoda kombinuje ideje metode polovljenja i secirajuće tačke. Umjesto uzimanja sredine intervala, koristi se tačka gdje prava koja povezuje vrijednosti funkcije na krajevima intervala siječe x-osu. Ta tačka postaje novi kraj intervala.");
     } else if (metoda === "polovljenje") {
       setNaslov("Metoda Polovljenja Intervala");
-      setDescription("Ova metoda se koristi za numeričko rešavanje jednačina i zasniva se na svojstvu neprekidnih funkcija da menjaju znak na intervalu gde funkcija ima koren. Metoda deli interval na pola i zadržava polovinu koja sadrži koren, sve dok ne dostigne zadatu tačnost.");
+      setDescription("Ova metoda se koristi za numeričko rješavanje jednačina i zasniva se na svojstvu neprekidnih funkcija da mijenjaju znak na intervalu gdje funkcija ima korjen. Metoda dijeli interval na pola i zadržava polovinu koja sadrži korjen, sve dok ne dostigne zadatu tačnost.");
     } else if (metoda === "prosta-iteracija") {
       setNaslov("Metoda Proste Iteracije");
-      setDescription("Metoda proste iteracije je numerički algoritam za rešavanje jednačina. Ideja je iterativno poboljšavati pretpostavljeno rešenje. Na osnovu početne tačke, koristi se iterativna formula da bi se dobile naredne vrednosti, ažurirajući pretpostavljeno rešenje sve dok ne dostignemo dovoljnu preciznost.");
+      setDescription("Metoda proste iteracije je numerički algoritam za rješavanje jednačina. Ideja je iterativno poboljšavati pretpostavljeno rešenje. Na osnovu početne tačke, koristi se iterativna formula da bi se dobile naredne vrijednosti, ažurirajući pretpostavljeno rešenje sve dok ne dostignemo dovoljnu preciznost.");
     } else if (metoda === "newton") {
       setNaslov("Newton-Raphsonova Metoda");
-      setDescription("Newton-Raphsonova metoda je iterativni numerički algoritam za aproksimaciju korena realne funkcije. Metoda zahteva početnu pretpostavku i iterativno konvergira ka tačnom rešenju, koristeći tangenti liniju funkcije.");
+      setDescription("Newton-Raphsonova metoda je iterativni numerički algoritam za aproksimaciju korjena realne funkcije. Metoda zahteva početnu pretpostavku i iterativno konvergira ka tačnom rešenju, koristeći tangenti liniju funkcije.");
     } else if (metoda === "modifikovan-newton") {
       setNaslov("Modifikovana Newton-Raphsonova Metoda");
-      setDescription("Modifikovana Newton-Raphsonova metoda je varijacija Newton-Raphsonove metode koja uključuje korekciju ako se iteracija približava ekstremu funkcije. Ovo poboljšava konvergenciju i stabilnost metode.");
+      setDescription("Modifikovana Newton-Raphsonova metoda bi u svakom koraku koristila samo prvi izvod funkcije u toj tački kako bi izračunala novu pretpostavku. Važno je napomenuti da ova metoda može zahtijevati pažljiviji odabir početne vrijednosti kako bi bila efikasna.");
     }
   }, [metoda]);
 
